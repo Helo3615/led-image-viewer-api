5ae606b48ba38cc6767a371fc06c1bf4d63115a9
Helo <helorigouin@gmail.com>
2020-04-19 12:25:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+import { displayText } from './displayFunctions'
+import { main } from './sets'
+
 const express = require('express')
 const app = express()
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
 })
-
-import { displayText } from './displayFunctions'
-import { main } from './sets'
 
 // routes
 app.get('/launch/set/main', (req, res) => main())
